5ddfa7fc15193635307f372f39e739e9c0c923b1
satooru65536 <ryuto4619@gmail.com>
2024-04-09 17:47:43 +0900

    feat: フッター

--- src/components/base/footer/index.tsx
+++ src/components/base/footer/index.tsx
@@ -3,7 +3,11 @@ import { ReactElement } from 'react';
 import styles from './index.module.scss';
 
 const Footer = (): ReactElement => {
-  return <footer className={styles.footer}>This is Footer</footer>;
+  return (
+    <footer className={styles.footer}>
+      <p>SatooRu © 2024 Copyright.</p>
+    </footer>
+  );
 };
 
 export default Footer;
