3f0b3b32700562505babe57190eb870aa48a147f
satooru65536 <ryuto4619@gmail.com>
2024-04-13 01:08:56 +0900

    refactor: iconCard のstyle指定方法

--- src/elements/iconCard/index.tsx
+++ src/elements/iconCard/index.tsx
@@ -11,14 +11,11 @@ interface Props {
 
 const IconCard = (props: Props): ReactElement => {
   const { iconKey, size } = props;
-  const cardSize = size === 'lg' ? '120px' : '80px';
-  const iconSize = size === 'lg' ? 48 : 32;
-  const fontSize = size === 'lg' ? '1rem' : '0.75rem';
 
   return (
-    <div className={styles.icon} style={{ width: cardSize }}>
-      <Icon size={iconSize} iconKey={iconKey} />
-      <p style={{ fontSize }}>{IconMap[iconKey].name}</p>
+    <div className={styles.icon} data-size={size}>
+      <Icon size={size === 'lg' ? 48 : 32} iconKey={iconKey} />
+      <p>{IconMap[iconKey].name}</p>
     </div>
   );
 };
