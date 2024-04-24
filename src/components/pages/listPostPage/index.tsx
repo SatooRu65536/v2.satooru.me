3c95e03ad8a4cc0c3b175657b6adfe5579d2aee5
satooru65536 <ryuto4619@gmail.com>
2024-04-24 11:31:21 +0900

    feat: 投稿一覧ページ

--- src/components/pages/listPostPage/index.tsx
+++ src/components/pages/listPostPage/index.tsx
@@ -2,10 +2,12 @@
 
 import dayjs from 'dayjs';
 import { useSearchParams } from 'next/navigation';
+import { useCallback } from 'react';
 
 import ListPostCard from './card';
 import styles from './index.module.scss';
 
+import PageNation from '@/components/shares/pagenation';
 import { PER_PAGE } from '@/const/setting';
 import { GetArticle } from '@/utils/articles';
 
@@ -22,19 +24,38 @@ const ListPostPage = (props: Props) => {
   const searchParams = useSearchParams();
   const page = parseInt(searchParams.get('p') ?? '1');
 
-  const displayedArticles = groupedArticles[category ?? 'others']?.slice((page - 1) * PER_PAGE, page * PER_PAGE);
+  const allArticles = category ? groupedArticles[category] ?? [] : articles;
+  const displayedArticles = allArticles.slice((page - 1) * PER_PAGE, page * PER_PAGE);
+
+  const pageTo = useCallback(
+    (n: number) => {
+      const path = category ? `/posts/${category}` : '/posts';
+      return `${path}?p=${n}`;
+    },
+    [category],
+  );
+
+  if (displayedArticles === undefined || displayedArticles.length === 0) {
+    return <div>記事はありません</div>;
+  }
 
   return (
     <div className={styles.container}>
-      {displayedArticles?.map(({ article, postedAt }) => (
-        <ListPostCard
-          key={article.data.number}
-          title={article.data.title}
-          content={article.content}
-          date={dayjs(postedAt)}
-          link={`/posts/${article.data.number}`}
-        />
-      ))}
+      <section className={styles.articles}>
+        {displayedArticles?.map(({ article, postedAt }) => (
+          <ListPostCard
+            key={article.data.number}
+            title={article.data.title}
+            content={article.content}
+            date={dayjs(postedAt)}
+            link={`/posts/${article.data.number}`}
+          />
+        ))}
+      </section>
+
+      <section className={styles.pagenation}>
+        <PageNation currentPage={page} totalArticles={allArticles.length} pageTo={pageTo} />
+      </section>
     </div>
   );
 };
---
+++ src/components/shares/pagenation/index.tsx
@@ -0,0 +1,68 @@
+import Link from 'next/link';
+import { ReactElement } from 'react';
+
+import styles from './index.module.scss';
+
+import { PER_PAGE } from '@/const/setting';
+
+interface Props {
+  totalArticles: number;
+  currentPage: number;
+  pageTo: (page: number) => string;
+}
+
+const DISPLAY_PAGE_RANGE = 2;
+
+const PageNation = (props: Props): ReactElement => {
+  const { totalArticles, currentPage, pageTo } = props;
+
+  const pages = Array.from({ length: Math.ceil(totalArticles / PER_PAGE) }, (_, i) => i + 1);
+  const rangeMin = Math.max(0, currentPage - DISPLAY_PAGE_RANGE - 1);
+  const rangeMax = Math.min(pages.length, currentPage + DISPLAY_PAGE_RANGE);
+  const displayPages = pages.slice(rangeMin, rangeMax);
+
+  const isFirstPageDisplayed = displayPages.at(0) === pages.at(0);
+  const isLastPageDisplayed = displayPages.at(-1) === pages.at(1);
+
+  return (
+    <div className={styles.pagenation}>
+      {!isFirstPageDisplayed && (
+        <>
+          <Link
+            key={pages.at(0)}
+            href={pageTo(pages.at(0) ?? 1)}
+            className={styles.page_link}
+            data-current={currentPage === pages.at(0)}
+          >
+            {pages.at(0)}
+          </Link>
+
+          <div className={styles.leader}>•••</div>
+        </>
+      )}
+
+      {displayPages.map((page) => (
+        <Link key={page} href={pageTo(page)} className={styles.page_link} data-current={page === currentPage}>
+          {page}
+        </Link>
+      ))}
+
+      {!isLastPageDisplayed && (
+        <>
+          <div className={styles.leader}>•••</div>
+
+          <Link
+            key={pages.at(-1)}
+            href={pageTo(pages.at(-1) ?? 1)}
+            className={styles.page_link}
+            data-current={currentPage === pages.at(-1)}
+          >
+            {pages.at(-1)}
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default PageNation;
