479fb687d50495d3f61c1d8faab4ba59c0247847
satooru65536 <ryuto4619@gmail.com>
2024-04-24 15:27:21 +0900

    feat: 全記事一覧ページ追加r

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,17 @@
+import { ReactElement, Suspense } from 'react';
+
+import ListPostPage from '@/components/pages/listPostPage';
+import Loading from '@/elements/loading';
+import { getArticles } from '@/utils/articles';
+
+const { articles } = getArticles();
+
+const Page = (): ReactElement => {
+  return (
+    <Suspense fallback={<Loading />}>
+      <ListPostPage articles={articles} category={undefined} />
+    </Suspense>
+  );
+};
+
+export default Page;
